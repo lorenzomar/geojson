c5298653a0da1f71f89f0a5eb6f76169ac5f78db
Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
2018-01-19 16:18:56 +0100

    Added base exceptions classes

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the GeoJSON package.
+ *
+ * (c) Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GeoJSON\Exception;
+
+/**
+ * Interface ExceptionInterface.
+ *
+ * @package GeoJSON
+ * @author Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ * @link https://github.com/lorenzomar/geojson
+ */
+interface ExceptionInterface
+{
+
+}
